385f7b0e1ae4a173ae6703d8441510f38bd40fd0
Pablo Geraldo <pdgeraldo@server.ucla-biostat-203b.com>
2021-03-06 04:57:46 +0000

    Shiny app skeleton

--- hw3/mimiciv_shiny/mimic-iv-visz/app.R
+++ hw3/mimiciv_shiny/mimic-iv-visz/app.R
@@ -7,32 +7,181 @@ library(shiny) # Basic shiny functionality
 library(shinydashboard) # Additional functions and layouts
 library(tidyverse) # For data manipulation inside the app
 
-# Load the dataset
-path <- "/home/pdgeraldo/biostat-203b-2021-winter/hw3/mimiciv_shiny"
-mimic <- readRDS(paste0(path,"/icu_cohort.rds"))
 
 # Describe the desired UI
 # Here I'm using the shinydasboard::dashboardPage
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
-)
-# Code generating the output plot
-server <- function(input, output){
+  
+  # Describe the header
+  dashboardHeader(title="Exploring MIMIC-IV data"),
+  
+  # Describe the sidebar (inputs)
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Demographics", tabName = "Demog"),
+      menuItem("Laboratory", tabName = "Labs"),
+      menuItem("Vital Charts", tabName = "Charts")
+    )
+  ),
+  
+  # Describe the body (output placeholder)
+  dashboardBody(
+    # Declare tab items
+    tabItems(
+      
+      # First tab: Demographics
+      tabItem(tabName = "Demog",
+              fluidRow(
+                # Create first column
+                column(width = 4,
+                # Box for selectInput
+                box(title = "Input selection",
+                    solidHeader = TRUE,
+                    status = "primary",
+                    width = NULL,
+                    #background = "light-blue",
+                    selectInput(inputId = "dvar",
+                                label = "Select demographic variable:",
+                                choices = c("gender",
+                                            "anchor_age",
+                                            "insurance",
+                                            "language",
+                                            "ethnicity"
+                                )) # Close selectInput
 
-    output$labsPlot <- renderPlot({
+                ), # Close box for selectInput
+                
+                box(title = "Missing Data", 
+                    width = NULL, 
+                    background = "maroon",
+                    "A box with a solid maroon background")
+                
+                ), # Close column
+                
+                #Box for plotOutput
+                box(title = "Variable Distribution",
+                    solidHeader = TRUE,
+                    status = "primary",
+                    width = 8,
+                  plotOutput("demplot")),
+              ) # Close fluidRow
+      ), # Close tabItem: Demographics
       
-      mimic %>%
-        dplyr::select(!!input$var) %>%
-        dplyr::filter(!is.na(!!input$var)) %>%
-        ggplot(aes(x = !!input$var)) +
-        geom_histogram() +
-        facet_wrap(. ~!!input$group) +
-        theme_minimal()
+      # Second tab: Labs
+      tabItem(tabName = "Labs",
+              fluidRow(
+                # Box for selectInput
+                box(title = "Input selection",
+                    solidHeader = TRUE,
+                    width = 4,
+                    background = "light-blue",
+                    selectInput(inputId = "lvar",
+                                label = "Select lab measure:",
+                                choices = c("bicarbonate","calcium",
+                                            "chrloride","createnine",
+                                            "glucose","magnesium",
+                                            "potassium","sodium",
+                                            "hematocrit","wbc",
+                                            "lactate")) # Close selectInput
+                ), # Close box selectInput
+                #Box for plotOutput
+                box(title = "Variable Distribution",
+                    width = 8,
+                    plotOutput("labplot"))
+              ) # Close fluidRow
+      ), # Close tabItem: Labs
       
-    })
+      # Third tab: Vitals
+      tabItem(tabName = "Charts",
+              fluidRow(
+                # Box for selectInput
+                box(title = "Input selection",
+                    solidHeader = TRUE,
+                    width = 4,
+                    background = "light-blue",
+                    selectInput(inputId = "cvar",
+                                label = "Select vitals measure:",
+                                choices = c("heart_rate",
+                                            "non_invasive_blood_pressure_systolic",
+                                            "non_invasive_blood_pressure_mean",
+                                            "respiratory_rate",
+                                            "temperature_fahrenheit"
+                                            
+                                )) # Close selectInput
+                ), # Close box selectInput
+                #Box for plotOutput
+                box(title = "Variable Distribution",
+                    width = 8,
+                    plotOutput("chartplot"))
+              ) # Close fluidRow
+      ) # Close tabItem: Vitals
+      
+    ) # Close tabItems
+    
+  ) # Close dashboardBody
+) # Close dashboardPage
+
+# Code generating the output plot
+server <- function(input, output) {
+  
+  # Load the dataset
+  path <- "/home/pdgeraldo/biostat-203b-2021-winter/hw3/mimiciv_shiny"
+  mimic <- readRDS(paste0(path,"/icu_cohort.rds"))
+  
+  ## ---- DEMOGRAPHICS ---- ##
+  # Reactive data for Demographics
+  dnas <- reactive({
+    mimic %>% 
+      dplyr:summarise(n = is.na(!!!input$dvar))
+  })
+  
+  ddata <- reactive({
+    mimic %>%
+      tidyr::drop_na(!!!input$dvar) %>%
+      dplyr::select(!!!input$dvar)
+  })
+  
+  # Creating demplot
+  output$demplot <- renderPlot(
+    
+    ggplot(data = ddata(), 
+           aes(x = ddata()[[input$dvar]], 
+               y = (..count..)/sum(..count..))) +
+      geom_bar(aes(fill = ddata()[[input$dvar]])) +
+      theme_bw() +
+      scale_y_continuous(breaks = seq(0, 1, by = 0.1),
+                         labels = scales::percent) +
+      theme(axis.text.x = element_blank(),
+            axis.ticks.x = element_blank())
+  )
+  
+  # Creating labplot
+  output$labplot <- renderPlot(
+    hist(rnorm(100, 25, 5))
+  )
+  
+  ## ---- CHART EVENTS ---- ##
+  # Reactive data for vitals
+  cnas <- reactive({
+    mimic %>% 
+      dplyr:summarise(n = is.na(!!!input$cvar))
+  })
+  
+  cdata <- reactive({
+    mimic %>%
+      tidyr::drop_na(!!!input$cvar) %>%
+      dplyr::select(!!!input$cvar)
+  })
+  
+  # Creating chartplot
+  output$chartplot <- renderPlot(
+    ggplot(data = cdata(), 
+           aes(y = cdata()[[input$cvar]])) +
+      geom_boxplot() +
+      theme_bw() 
+  )
+    
 }
 
 # Run the application 
